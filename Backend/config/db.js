5b05bec597f365cf48d0f0b3ea0cd71d7c9b52a8
Noura Ismail <145156745+nouraismaill@users.noreply.github.com>
2024-09-23 12:20:10 +0300

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,19 +1,23 @@
-import dotenv from "dotenv";
-import mysql from "mysql2";
-dotenv.config();
-const connectDB = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 3306 
-});
-connectDB.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err.stack);
-    return;
-  }
-  console.log("DB is connected");
-});
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
-export default connectDB;
+dotenv.config();
+
+async function connectDB() {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 3306
+    });
+    console.log("DB is connected");
+    return connection;
+  } catch (err) {
+    console.error("Error connecting to the database:", err.message);
+  }
+}
+
+connectDB();
+
