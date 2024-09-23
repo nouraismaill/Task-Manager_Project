7d78e3b51943ac71f49a1c6c99522000c516abe0
Noura Ismail <145156745+nouraismaill@users.noreply.github.com>
2024-09-23 13:01:35 +0300

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import mysql from "mysql2";
+
 dotenv.config();
+
 const connectDB = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -8,9 +10,11 @@ const connectDB = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
+console.log("Attempting to connect to MySQL database...");
+
 connectDB.connect((err) => {
   if (err) {
-    console.error("Error connecting to the database:", err.stack);
+    console.error("Error connecting to the database:", err.message, err.stack);
     return;
   }
   console.log("DB is connected");
