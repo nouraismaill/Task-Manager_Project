648372510eaf1407eb60bb3a64448f177edba35c
Noura Ismail <145156745+nouraismaill@users.noreply.github.com>
2024-09-23 03:37:53 +0300

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,6 +6,7 @@ const connectDB = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: 3306 
 });
 connectDB.connect((err) => {
   if (err) {
